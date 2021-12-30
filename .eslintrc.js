dc8c38a5d2d14544b592d800e4b062596ac5f8cb
Juan Daniel Martinez <jdmartinez@itparral.edu.mx>
2021-12-29 19:06:01 -0700

    * ESlint and Prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'next',
+    'prettier',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'warn',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/prop-types': 'warn',
+    'react/jsx-one-expression-per-line': 'off',
+  },
+};
