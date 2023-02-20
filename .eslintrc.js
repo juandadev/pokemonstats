b5469e2de8a9b160fc3b40bf7972d3e9c6d9349c
Juan Daniel Martinez <jdmartinez@itparral.edu.mx>
2023-02-20 11:25:05 -0600

    Updating eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,11 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'next',
     'prettier',
-    'airbnb',
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -16,19 +17,17 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-props-no-spreading': 'warn',
-    'react/prop-types': 'warn',
-    'react/jsx-one-expression-per-line': 'off',
-    'import/prefer-default-export': 'warn',
-    'react/no-array-index-key': 'warn',
-    'no-param-reassign': 'warn',
-    camelcase: 'warn',
-    'jsx-a11y/no-static-element-interactions': 'warn',
-    'jsx-a11y/click-events-have-key-events': 'warn',
+    'react/jsx-filename-extension': ['off'],
+    'react/jsx-props-no-spreading': ['warn'],
+    'react/prop-types': ['warn'],
+    'react/jsx-one-expression-per-line': ['off'],
+    'import/prefer-default-export': ['warn'],
+    'react/no-array-index-key': ['warn'],
+    'no-param-reassign': ['warn'],
+    camelcase: ['warn'],
+    'jsx-a11y/no-static-element-interactions': ['warn'],
+    'jsx-a11y/click-events-have-key-events': ['warn'],
   },
 };
