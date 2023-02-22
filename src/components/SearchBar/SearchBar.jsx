9f2d8442abdd7fb28c00ed3b0af1d3942a76f2b2
Juan Martinez <jdmartinez@itparral.edu.mx>
2023-02-22 14:45:56 -0600

    Adding a dropdown to search input for suggestions. Adding the first exception for nidoran

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,16 @@
 import React, { useContext, useState } from 'react';
-import { FormControl, FormText, InputGroup } from 'react-bootstrap';
+import {
+  FormControl,
+  FormText,
+  InputGroup,
+  ListGroup,
+  ListGroupItem,
+} from 'react-bootstrap';
 import { pokemon } from '../../context';
+import s from './SearchBar.module.scss';
 
 // TODO: Need to add exceptions for some pokemons with different name in the API, mostly those with variants.
 /*
-Nidoran Male and Female
 Lycanrock and its different forms
 Pumpkaboo
 Perrserker
@@ -24,6 +30,8 @@ Some legendary pokemons like zacian and zamazenta
 
 export default function SearchBar() {
   const [field, setField] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [dropdownList, setDropdownList] = useState([]);
   const { dispatch } = useContext(pokemon);
 
   const handleChange = (event) => {
@@ -32,35 +40,84 @@ export default function SearchBar() {
     setField(value);
   };
 
+  const filterException = (value) => {
+    const sanitizedValue = value
+      .toLowerCase()
+      .trim()
+      .replaceAll(/[\s.]+/g, '-');
+
+    switch (sanitizedValue) {
+      case 'nidora':
+        setDropdownList([
+          { name: 'Nidoran ♂', id: 32 },
+          { name: 'Nidoran ♀', id: 29 },
+        ]);
+        setShowDropdown(true);
+        break;
+
+      default:
+        setDropdownList([]);
+        setShowDropdown(false);
+        return sanitizedValue;
+    }
+  };
+
   const handleKeyDown = (event) => {
     const { target, key } = event;
     const { value } = target;
+    const filteredValue = filterException(value);
 
     if (key === 'Enter') {
       dispatch({
         type: 'CHANGE_INPUT',
-        name: value
-          .toLowerCase()
-          .trim()
-          .replaceAll(' ', '-'),
+        name: filteredValue,
       });
       setField('');
     }
   };
 
+  const handleDropdownItemClick = (event) => {
+    const { id } = event.target.dataset;
+
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name: id,
+    });
+    setField('');
+    setShowDropdown(false);
+    setDropdownList([]);
+  };
+
+  const renderDropdownItems = () =>
+    dropdownList.map(({ name, id }) => (
+      <ListGroupItem
+        key={`search-${id}`}
+        data-id={id}
+        action
+        onClick={handleDropdownItemClick}
+      >
+        {name}
+      </ListGroupItem>
+    ));
+
   return (
     <>
-      <InputGroup>
-        <InputGroup.Text id="searchPokemon">Search</InputGroup.Text>
-        <FormControl
-          placeholder="Type name or code"
-          aria-label="pokemon"
-          aria-describedby="searchPokemon"
-          value={field}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-        />
-      </InputGroup>
+      <div className={s.searchContainer}>
+        <InputGroup>
+          <InputGroup.Text id="searchPokemon">Search</InputGroup.Text>
+          <FormControl
+            placeholder="Type name or code"
+            aria-label="pokemon"
+            aria-describedby="searchPokemon"
+            value={field}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+        </InputGroup>
+        {showDropdown && (
+          <ListGroup className={s.dropdown}>{renderDropdownItems()}</ListGroup>
+        )}
+      </div>
       <FormText id="searchPokemon" className="mb-3">
         Type the pokemon name or code, then hit <code>Enter</code>
       </FormText>
