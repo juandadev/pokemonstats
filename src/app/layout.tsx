1b1bfb517654496f8033ec169e9e8cce19b5c1c4
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2025-06-30 19:28:18 -0600

    feature: Add Tailwind CSS integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 import { Metadata } from 'next';
 import { ContextProvider } from '../context';
 import { Container } from 'react-bootstrap';
-import '../styles/globals.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Pokémon stats',
   description: 'An useful tool for your pokémon adventures.',
 };
-
-// TODO: Configure fonts:
-// <link rel="preconnect" href="https://fonts.googleapis.com" />
-// <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-// <link
-//   href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
-//   rel="stylesheet"
-// />
 
 export default function RootLayout({
   children,
